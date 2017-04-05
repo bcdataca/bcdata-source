1065029b655bedfde25fd0ab49f05fbfc9ec456e
Aaron Berk <aaron.riot1@gmail.com>
2017-04-05 14:02:50 -0700

    organizational stuff

---
+++ scripts/publi.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+
+echo "Generating site"
+hugo
+
+cp CNAME ./public/
+cp -r favi/* ./public/
